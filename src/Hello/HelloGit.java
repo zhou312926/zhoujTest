5061531afb92f132b347bda7ef944b3f333c6e2d
zhou312926 <mathew_zhou@163.com>
2019-04-28 20:13:27 +0800

    First commit all zhouj 3333;

--- src/Hello/HelloGit.java
+++ src/Hello/HelloGit.java
@@ -7,5 +7,7 @@ public class HelloGit {
         System.out.println("3333333333333");
         System.out.println("等待pull");
         System.out.println("0428");
+        System.out.println("11111111111111");
+
     }
 }
